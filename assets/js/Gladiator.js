0419b480a382a46c47cd5e61ca3442e37c06d818
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-12-04 12:57:46 -0600

    drew full body for gladiator

--- assets/js/Gladiator.js
+++ assets/js/Gladiator.js
@@ -31,14 +31,29 @@ class Gladiator {
     }
 
     
-    // draw method to animate the gladiator
+    // draw method to draw the gladiator
     draw(ctx) {
         ctx.beginPath();
-        ctx.moveTo(50, 50);
-        ctx.arc(50, 50, Math.PI, 50, 0, true);
-        // ctx.lineTo(100, 75);
-        // ctx.lineTo(100, 25);
 
-        ctx.fill();
+        // draw the head
+        ctx.arc(50, 50, Math.PI * 5, Math.PI * 2, 0, true);
+
+        // draw the body
+        ctx.moveTo(50, 50 + Math.PI * 5);
+        ctx.lineTo(50, 100 + Math.PI * 5);
+
+        // draw the arms
+        ctx.moveTo(50, 75 + Math.PI * 5);
+        ctx.lineTo(70, 60 + Math.PI * 5);
+        ctx.moveTo(50, 75 + Math.PI * 5);
+        ctx.lineTo(30, 60 + Math.PI * 5);
+
+        // draw the legs
+        ctx.moveTo(50, 100 + Math.PI * 5);
+        ctx.lineTo(70, 115 + Math.PI * 5);
+        ctx.moveTo(50, 100 + Math.PI * 5);
+        ctx.lineTo(30, 115 + Math.PI * 5);
+
+        ctx.stroke();
     }
 }
